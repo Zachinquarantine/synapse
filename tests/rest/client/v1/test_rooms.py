0bf10cf80d73f2d84d6ff47ce8caa2fa1c16e7dc
Patrick Cloke <patrickc@matrix.org>
2021-04-08 13:50:25 -0400

    Run lint scripts.

--- tests/rest/client/v1/test_rooms.py
+++ tests/rest/client/v1/test_rooms.py
@@ -19,9 +19,8 @@
 """Tests REST events for /rooms paths."""
 
 import json
-from urllib import parse as urlparse
-
 from unittest.mock import Mock
+from urllib import parse as urlparse
 
 import synapse.rest.admin
 from synapse.api.constants import EventContentFields, EventTypes, Membership
